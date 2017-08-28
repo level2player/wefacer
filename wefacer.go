0ff6205c3bd88ce1ac52594cc45d3a20955ded5c
zhangjie12497 <576102544@qq.com>
2017-08-28 12:07:56 +0800

    20170828-v1

--- wefacer.go
+++ wefacer.go
@@ -8,6 +8,7 @@ import (
 )
 
 func main() {
+	log.Println("start wefacer ...")
 	http.HandleFunc("/", wechatservices.ReceiveRequest)                       //设置访问的路由
 	err := http.ListenAndServe(":"+core.WefacerConfig.ConfigMap["port"], nil) //设置监听的端口
 	if err != nil {
