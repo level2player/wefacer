b535a58fb93ef8eba47625f04305706f5d395446
level2player <576102544@qq.com>
2018-04-24 15:39:47 +0800

    20180424

--- wefacer.go
+++ wefacer.go
@@ -5,9 +5,26 @@ import (
 	"net/http"
 	"wefacer/core"
 	"wefacer/wechat/wechatservices"
+	"github.com/spf13/cobra"
+	"os"
 )
+var RootCmd = &cobra.Command{
+    Use: "-v",
+    Run: func(cmd *cobra.Command, args []string) {
+        println("wefacer version is 0.0.1")
+	},
+}
+var UpCmd= &cobra.Command{
+	Use: "up",
+    Run: func(cmd *cobra.Command, args []string) {
+        LoadWefacer()
+	},
+}
+func init(){
+    RootCmd.AddCommand(UpCmd)
+}
 
-func main() {
+func LoadWefacer(){
 	core.Print_log("start wefacer ...")
 	http.HandleFunc("/", wechatservices.ReceiveRequest)
 	err := http.ListenAndServe(":"+core.WefacerConfig.ConfigMap["port"], nil)
@@ -15,4 +32,11 @@ func main() {
 		core.Print_log(err.Error())
 		log.Fatal("ListenAndServe: ", err)
 	}
+}
+
+func main() {
+    if err := RootCmd.Execute(); err != nil {
+        log.Println(err)
+        os.Exit(1)
+    }
 }
